6f1317a40642334f6ba3b068dd6171a5f7ba52c5
gias-uddin-swe <nirobh753@gmail.com>
2021-11-03 23:42:01 +0600

    done

--- index.js
+++ index.js
@@ -25,44 +25,72 @@ client.connect((err) => {
   const servicesCollection = client.db("travelWorld").collection("services");
   const bookingsCollection = client.db("travelWorld").collection("bookings");
 
-  // add Services
+  // ad service
+
   app.post("/addServices", async (req, res) => {
     const result = await servicesCollection.insertOne(req.body);
     res.send(result);
   });
 
-  //get servicesCollection
-
-  app.get("/services", async (req, res) => {
+  // get all service
+  app.get("/allServices", async (req, res) => {
     const result = await servicesCollection.find({}).toArray();
     res.send(result);
+    console.log(result);
   });
 
-  // get single service
-  app.get("/details/:id", async (req, res) => {
-    console.log(req.params.id);
+  // get single product
+  app.get("/singleProduct/:id", async (req, res) => {
     const result = await servicesCollection
       .find({ _id: ObjectId(req.params.id) })
       .toArray();
-    console.log(result);
     res.send(result[0]);
   });
 
-  // confirm booking service
-
-  app.post("/confirmBooking", async (req, res) => {
-    const result = bookingsCollection.insertOne(req.body);
+  // cofirm order
+  app.post("/confirmOrder", async (req, res) => {
+    const result = await bookingsCollection.insertOne(req.body);
     res.send(result);
   });
 
-  /// get my booking
-  app.get("/myBooking/:email", async (req, res) => {
+  // my confirmOrder
+
+  app.get("/myOrders/:email", async (req, res) => {
     const result = await bookingsCollection
-      .find({
-        email: req.params.email,
-      })
+      .find({ email: req.params.email })
       .toArray();
     res.send(result);
+  });
+
+  /// delete order
+
+  app.delete("/delteOrder/:id", async (req, res) => {
+    const result = await bookingsCollection.deleteOne({
+      _id: ObjectId(req.params.id),
+    });
+    res.send(result);
+  });
+
+  // all order
+  app.get("/allOrders", async (req, res) => {
+    const result = await bookingsCollection.find({}).toArray();
+    res.send(result);
+  });
+
+  // update statuses
+
+  app.put("/updateStatus/:id", (req, res) => {
+    const id = req.params.id;
+    const updatedStatus = req.body.status;
+    const filter = { _id: ObjectId(id) };
+    console.log(updatedStatus);
+    bookingsCollection
+      .updateOne(filter, {
+        $set: { status: updatedStatus },
+      })
+      .then((result) => {
+        res.send(result);
+      });
   });
 });
 
